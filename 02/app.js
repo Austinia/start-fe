9624ea746137c8dfb59ab2b9c0d49a027a97e3ad
Austinia <kimdy90919@gmail.com>
2022-03-10 17:29:55 +0900

    수업끝 커밋

--- 02/app.js
+++ 02/app.js
@@ -3,6 +3,7 @@
  *
  *
  * */
+alert("f12 -> Console");
 name = "austin";
 
 isMan = true;
@@ -37,6 +38,61 @@ var me = {
 };
 me.sayHello();
 
+var arr = ['You', 'Are', 'Cute', 'ㅇ.<'];
+
+// for (var i = 0; i < arr.length; i++) {
+//   log(arr[i]);
+// }
+arr.forEach(function(word){
+  log(word);
+})
+
+// Array CRUD
+//C
+// var todos = ['운동'];
+// var todo = '게임';
+// todos.push(todo);
+// console.log(todos);
+// //R
+// var todos = ['운동', '게임'];
+// todos.forEach(function(todo) {
+//   console.log(todo);
+// });
+//U
+// var todos = ['운동', '게임'];
+// var updateTodo = '게임';
+// var updateIndex = todos.findIndex(function(todo) {
+//   return todo === updateTodo;
+// });
+// todos[updateIndex] = '공부';
+// console.log(todos);
+
+// var todos = ['운동', '게임'];
+// var updateTodo = '게임';
+
+// var newTodos = todos.map(function(todo) {
+//   if (todo === updateTodo) {
+//     return '공부';
+//   }
+//   return todo;
+// });
+
+// console.log(newTodos);
+//D
+// var todos = ['운동', '공부', '목욕'];
+// var deleteTodo = '공부';
+// var deleteIndex = todos.findIndex(function(todo) {
+//   return todo === deleteTodo;
+// });
+// todos.splice(deleteIndex, 1);
+// console.log(todos);
+
+// var todos = ['운동', '공부'];
+// var deleteTodo = '공부';
+// var newTodos = todos.filter(function(toods) {
+//   return todo !== delteTodo;
+// });
+// console.log(newTodo);
 
 // var name = 'FE';
 // var age = 28;
