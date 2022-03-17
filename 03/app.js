102e4b2f7c9929e4cf3224f311e47de451e9c7f3
Austinia <kimdy90919@gmail.com>
2022-03-17 16:56:32 +0900

    첫 커밋

---
+++ 03/app.js
@@ -0,0 +1,207 @@
+function log(str) {
+  console.log(str);
+}
+
+var foo = 42;
+var foo = "bar";
+var foo = true;
+
+if ("test") {
+  //true로 자동전환
+  log("true");
+}
+log("2" + 2); // "22"
+log(2 + 5 + "1"); // "71"
+
+// 값
+var a = 1;
+var b = a;
+b = 2;
+console.log(a, b);
+
+//참조
+var a = [1];
+var b = a;
+b[0] = [2];
+console.log(a, b);
+
+var a = { x: 1 };
+var b = a;
+b.x = 2;
+console.log(a, b);
+
+//call by value
+function byValue(count) {
+  count = count + 2;
+}
+var count = 3;
+byValue(count);
+log(count); //3
+
+//call by reference
+function byReference(count) {
+  count.push("2");
+}
+var count = ["1"];
+byReference(count);
+log(count); // ['1', '2']
+
+//if
+isMan = true;
+if (isMan) {
+  log("i am man");
+} else {
+  log("am i woman?");
+}
+
+var gender = isMan ? "남자" : "여자";
+
+//for
+for (var count = 0; count < 5; count++) {
+  //5 times
+}
+var obj = { a: 1, b: 2 };
+
+//Slow
+for (var prop in obj) {
+  console.log(prop, obj[prop]);
+}
+
+//while
+var isClosed = true;
+while (isClosed) {
+  //repeat
+
+  //Closed = false;
+
+  //break
+  break;
+}
+
+//function
+function sum(x) {
+  var y = 1;
+  return x + y; //no return = undefined
+}
+//same
+var sum = function() {
+  var y = 1;
+  return x + y;
+}
+
+try{
+//if it catch the error move to catch
+}catch(e){
+//Only Error happend
+}finally{
+//Must run
+}
+
+//Use frequency
+// alert('alert');
+
+// var who = prompt('Who R U?');
+// var isDelete = confirm('R U SURE to DELETE?');
+var enc = encodeURIComponent('카카오');
+decodeURIComponent(enc);
+
+isNaN('1')//has number?
+parseInt('12');
+parseInt('09', 10);
+parseFloat('10.33');
+
+function test() {
+  console.log('test()');
+}
+
+// var id = setTimeout(test, 1000); //1초후 1번 실행
+// var id = setInterval(test, 1000) //1초마다 실행
+
+//setInterval 보다 나은 방법
+function test() {
+  console.log('test()');
+  setTimeout(test, 1000);
+}
+
+// clearTimeout(id);
+// clearInterval(id);
+
+//Scope
+var nick = 'aji'//global
+
+function test(){
+  var age = 30;//local
+  console.log(nick, age);
+}
+
+test();
+// console.log(age); //error
+
+//Hoisting
+var name = 'global';
+function test() {
+  console.log(name);
+  var name = 'local';
+  console.log(name);
+}
+
+test2();
+function test2(){
+  console.log('test');
+}
+
+//function
+var car = {
+  year: '2022',
+  starting: function() {
+    console.log('starting..');
+  },
+}
+
+car.starting();
+
+//익명 함수표현식
+var car = function() {
+
+}
+
+//중첩함수
+
+//익명 즉시실행함수, 스코프 보호
+// (function() {
+
+// })();
+
+
+
+var sums = 0;
+var counts = 0;
+var count = [10,20,30,40,50,60,70,80,90,100];
+count.forEach(element => {
+  sums += element;
+  counts += 1;
+});
+var result = sums / counts;
+log(result)
+
+// for(i = 1; i < 10; i++){
+//   for(j = 1; j < 10; j++){
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
+
+function gogodan(i){
+    for(j = 1; j < 10; j++){
+      console.log(`${i} * ${j} = ${i * j}`);
+    }
+}
+
+var Obj = {
+  do: function() {
+    console.log('Hello from Method');
+  }
+}
+
+var number1 = prompt('1번째 숫자');
+var number2 = prompt('2번째 숫자');
+alert(parseInt(number1) + parseInt(number2));
